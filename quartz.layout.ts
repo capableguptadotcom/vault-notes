1e70510ce33758a195c867e2040a21d9d5f600c6
Saksham <guptasaksham1611@gmail.com>
2026-05-25 11:53:35 -0700

    Show profile avatar and simplify footer contact

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,7 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      RSS: "/index.xml",
+      X: "https://x.com/capablegupta",
     },
   }),
 }
